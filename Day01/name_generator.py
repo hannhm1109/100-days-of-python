f0a157bed5060eb167dee4967b97fbc52daac967
hannhm1109 <92451044+hannhm1109@users.noreply.github.com>
2022-04-05 15:52:52 +0000

    Add files via upload

---
+++ Day01/name_generator.py
@@ -0,0 +1,4 @@
+print("welcome to the band name generator !")
+name = input("what's the name of the city you grow up in ? \n")
+pet = input("what's the name of your pet ?\n")
+print("your bad name could be :" + name +" "+pet)
